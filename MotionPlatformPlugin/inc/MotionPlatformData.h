f78bee8de51c26d3dc73d88ff60733804c17853e
Jonathan_Laptop <jonplumpton@gmail.com>
2013-04-16 17:15:32 -0400

    First Commit
    -Add FlightDataReaderConsole
    -Add FlightInputReplayPlugin (not configured)
    -Add MotionPlatformData (not configured)

---
+++ MotionPlatformPlugin/inc/MotionPlatformData.h
@@ -0,0 +1,14 @@
+/*
+Plugin to show how to derive motion platform data from our datarefs
+Thanks to Austin for allowing us to use the original Xplane conversion code.
+
+Version 1.0.0.1			Intitial Sandy Barbour - 05/08/2007
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include "../../external/XplaneSdk/CHeaders/xplm/XPLMDisplay.h"
+#include "../../external/XplaneSdk/CHeaders/xplm/XPLMGraphics.h"
+#include "../../external/XplaneSdk/CHeaders/xplm/XPLMProcessing.h"
+#include "../../external/XplaneSdk/CHeaders/xplm/XPLMDataAccess.h"
